98c06261d1ed179f01127deca759ea50e5b51943
nmxi <nagatsukimxi@gmail.com>
2020-04-03 16:59:08 +0900

    v1.0

---
+++ Assets/nmxi.towel/Scripts/PosSync.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosSync : MonoBehaviour
+{
+    [SerializeField] private Transform referenceTransform;
+
+    void Update()
+    {
+        gameObject.transform.position = referenceTransform.position;
+    }
+}
---
+++ Assets/nmxi.towel/Scripts/TowelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using VRM;
+
+public class TowelController : MonoBehaviour
+{
+    [SerializeField] private GameObject referenceHand;
+    [SerializeField] private VRMSpringBone vRMSpringBone;
+
+    [Space(15f), SerializeField] private GameObject towelBone1;
+
+    [SerializeField] private float thresold;
+
+    [Space(15f), SerializeField] private float lowStiffnessForce;
+    [SerializeField] private float highStiffnessForce;
+    [SerializeField] float lowDragForce;
+    [SerializeField] float highDragForce;
+
+    private VelocityEstimator velocityEstimator;
+
+    private void Awake()
+    {
+        velocityEstimator = referenceHand.GetComponent<VelocityEstimator>();
+    }
+
+    void Update()
+    {
+        if (velocityEstimator == null)
+            return;
+
+        towelBone1.transform.position = referenceHand.transform.position;
+
+        var acc = velocityEstimator.GetAccelerationEstimate();
+        var accAvr = Mathf.Abs((acc.x + acc.y + acc.z) / 3f);
+
+        //Debug.Log(accAvr);
+
+        if(accAvr < thresold)
+        {
+            vRMSpringBone.m_stiffnessForce = lowStiffnessForce;
+            vRMSpringBone.m_dragForce = lowDragForce;
+        }
+        else
+        {
+            vRMSpringBone.m_stiffnessForce = highStiffnessForce;
+            vRMSpringBone.m_dragForce = highDragForce;
+        }
+    }
+}
